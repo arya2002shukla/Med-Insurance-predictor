c0680af54d93d0dd75b9fe0606414ef2821c1183
arya2002shukla <85741754+arya2002shukla@users.noreply.github.com>
2023-05-23 22:17:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
+
 def load_css():
     with open("style.css") as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
