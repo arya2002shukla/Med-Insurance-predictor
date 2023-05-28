f4853fb44ce22a5f750467bcbf2918b3c6473948
arya2002shukla <85741754+arya2002shukla@users.noreply.github.com>
2023-05-28 10:46:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from sklearn.model_selection import train_test_split
 
 
 
+
 def load_css():
     with open("style.css") as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
